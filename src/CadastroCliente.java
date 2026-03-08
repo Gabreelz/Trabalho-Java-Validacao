54159c184d53e443eb8b67c7508a41043d8a795e
LipeProgramer <felipe.156888-2023@aluno.unicv.edu.br>
2026-03-08 20:59:10 -0300

    Adicionando validações de CPF e Email integradas com as classes utilitárias

--- src/CadastroCliente.java
+++ src/CadastroCliente.java
@@ -6,6 +6,20 @@ public class CadastroCliente {
     private static final String ARQUIVO_CLIENTES = "clientes.txt";
 
     public String cadastrar(String nome, String cpf, String email) {
-        return "Processando cadastro de: " + nome;
+        
+        if (nome == null || nome.isBlank()) {
+            return "Erro: O nome é obrigatório.";
+        }
+
+        if (!ValidacaoCPF.cpfValido(cpf)) {
+            return "Erro: CPF informado é inválido.";
+        }
+
+        String statusEmail = ValidacaoEmail.validar(email);
+        if (!statusEmail.equals("E-mail válido!")) {
+            return "Erro no e-mail: " + statusEmail;
+        }
+
+        return "Dados validados. Pronto para salvar.";
     }
 }
