acd129c2e8fea0e2b781b816584cdf3b9c4138c3
LipeProgramer <felipe.156888-2023@aluno.unicv.edu.br>
2026-03-08 20:47:08 -0300

    estrutura inicial da classe CadastroCliente

---
+++ src/CadastroCliente.java
@@ -0,0 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CadastroCliente {
+    private static final String ARQUIVO_CLIENTES = "clientes.txt";
+
+    public String cadastrar(String nome, String cpf, String email) {
+        return "Processando cadastro de: " + nome;
+    }
+}
