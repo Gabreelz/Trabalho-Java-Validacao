dfcd712ec1999cfe95d4aa1716d9dc0c56ac9fe1
Bruno Kenji <bruno.168899-2024@aluno.unicv.edu.br>
2026-03-09 03:49:22 -0300

    Relatorio do Cliente

---
+++ src/RelatorioCliente.java
@@ -0,0 +1,10 @@
+public class RelatorioCliente {
+
+    public static void imprimir(String nome, String cpf, String email) {
+        System.out.println("----- RELATÓRIO DO CLIENTE -----");
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("E-mail: " + email);
+        System.out.println("-------------------------------");
+    }
+}
