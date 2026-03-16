81d59643b820195ba08b297c9d695fe5f03d0cd0
Felip4n <arantes.felip25@gmail.com>
2026-03-15 22:15:54 -0300

    Feat: Mudando o arquivo principal do programa

--- Main.java
+++ Main.java
@@ -9,43 +9,54 @@ public class Main {
 
         boolean logado = false;
 
-     while (!logado) {
-
-            System.out.println(" LOGIN ");
-
-            System.out.print("Usuario: ");
-            String usuario = sc.nextLine();
-
-            System.out.print("Senha: ");
-            String senha = sc.nextLine();
-
-            if (Login.validarLogin(usuario, senha)) {
-                logado = true;
-                System.out.println("Login realizado com sucesso!");
-            } else {
-                System.out.println("Usuario ou senha invalidos.\n");
-            }
-        }
-
         int opcao;
 
         // MENU DO SISTEMA
         do {
-
-            System.out.println("\n===== MENU =====");
-            System.out.println("1 - Cadastrar Cliente");
-            System.out.println("2 - Ler Estados do CSV");
-            System.out.println("3 - Gerar Relatorio de Clientes");
+            System.out.println("\n===== MENU DE TESTES E SISTEMA =====");
+            System.out.println("1 - Testar apenas Validacao de E-mail");
+            System.out.println("2 - Testar apenas Validacao de CPF");
+            System.out.println("3 - Testar apenas Validacao de CNPJ");
+            System.out.println("4 - Testar Cadastro Completo de Cliente (Gravar no TXT)");
+            System.out.println("5 - Ler Estados do CSV");
+            System.out.println("6 - Gerar Relatorio de Clientes");
+            System.out.println("7 - Realizar Login");
             System.out.println("0 - Sair");
             System.out.print("Escolha uma opcao: ");
 
             opcao = sc.nextInt();
-            sc.nextLine();
+            sc.nextLine(); // Limpar o buffer do scanner
 
             switch (opcao) {
 
                 case 1:
+                    System.out.print("Digite o e-mail para testar: ");
+                    String emailTeste = sc.nextLine();
+                    String resultadoEmail = ValidacaoEmail.validar(emailTeste);
+                    System.out.println("Resultado da Validacao: " + resultadoEmail);
+                    break;
 
+                case 2:
+                    System.out.print("Digite o CPF para testar: ");
+                    String cpfTeste = sc.nextLine();
+                    if (ValidacaoCPF.cpfValido(cpfTeste)) {
+                        System.out.println("Resultado da Validacao: CPF Valido!");
+                    } else {
+                        System.out.println("Resultado da Validacao: CPF Invalido!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Digite o CNPJ para testar: ");
+                    String cnpjTeste = sc.nextLine();
+                    if (ValidacaoCNPJ.cnpjValido(cnpjTeste)) {
+                        System.out.println("Resultado da Validacao: CNPJ Valido!");
+                    } else {
+                        System.out.println("Resultado da Validacao: CNPJ Invalido!");
+                    }
+                    break;
+
+                case 4:
                     System.out.print("Nome: ");
                     String nome = sc.nextLine();
 
@@ -64,37 +75,43 @@ public class Main {
 
                     boolean isCnpj = (tipo == 2);
 
-                    String resultado = cadastro.cadastrar(nome, documento, email, isCnpj);
-
-                    System.out.println(resultado);
-
+                    String resultadoCadastro = cadastro.cadastrar(nome, documento, email, isCnpj);
+                    System.out.println(resultadoCadastro);
                     break;
 
-                case 2:
-
+                case 5:
                     LeitorCSV leitor = new LeitorCSV("estados.csv");
                     leitor.lerEstados();
-
                     System.out.println("CSV processado!");
-
                     break;
 
-                case 3:
-
+                case 6:
                     RelatorioCliente.mostrarClientes();
+                    break;
 
+                case 7:
+                    System.out.println(" LOGIN ");
+
+                    System.out.print("Usuario: ");
+                    String usuario = sc.nextLine();
+
+                    System.out.print("Senha: ");
+                    String senha = sc.nextLine();
+
+                    if (Login.validarLogin(usuario, senha)) {
+                        logado = true;
+                        System.out.println("Login realizado com sucesso!");
+                    } else {
+                        System.out.println("Usuario ou senha invalidos.\n");
+                    }
                     break;
 
                 case 0:
-
                     System.out.println("Sistema encerrado.");
-
                     break;
 
                 default:
-
                     System.out.println("Opcao invalida.");
-
             }
 
         } while (opcao != 0);
